b67345dbb623e06851de29910f5941a102b365ce
vedanthvdev <61700595+vedanthvdev@users.noreply.github.com>
2020-07-31 00:17:07 +0100

    Allignment

--- ElevatorAlgorithm/StartMain.cs
+++ ElevatorAlgorithm/StartMain.cs
@@ -2,12 +2,11 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace ElevatorAlgorithm
 {
-	
-class StartMain
+
+    class StartMain
     {
         static void Main(string[] args)
         {
